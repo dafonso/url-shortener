05588cc87733362c8920c7df820219540c4bbb0c
Diogo Afonso <dfdafonso@gmail.com>
2020-07-30 14:50:29 +0100

    change api throthling params + tweaks to kernel to better handle 404s on API requests

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -50,6 +51,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof ModelNotFoundException && $request->expectsJson()) {
+            return response()->json([
+                'message' => 'Not found',
+            ], 404);
+        }
+
         return parent::render($request, $exception);
     }
 }
