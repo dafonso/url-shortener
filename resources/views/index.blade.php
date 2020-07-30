e51c6e7b6753a7d08a92dc58f1699ee69161d3ef
Diogo Afonso <dfdafonso@gmail.com>
2020-07-30 19:34:36 +0100

    fix link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>URL Shortener</title>
-    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <script defer src="//use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
     <script defer src="{{ mix('/js/app.js') }}"></script>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.16.2/styles/github.min.css">
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
